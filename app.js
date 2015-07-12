00c459cd2af7cf06644607b3ca126829b87038ce
Jawn <jawnnypoo@gmail.com>
2015-07-12 15:40:13 -0500

    Switch over to using Polymer 1.0 for the site, and start with the Polymer starting kit. First pass of site done, adding the routing for the main sections

--- app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('polymer-ready', function() {
-	// Stuff that you want to run when the document is ready
-	// var navicon = document.getElementById('navicon');
-	// var drawerPanel = document.getElementById("drawerPanel");
-	// navicon.addEventListener('click', function() {
-	// 	drawerPanel.togglePanel();
-	// });
-});
