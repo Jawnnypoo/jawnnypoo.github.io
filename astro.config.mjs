991fa1213fbd1f1c6eefc872b88d96f858639470
John Carlson <jawnnypoo@gmail.com>
2026-05-21 18:45:04 -0500

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  vite: {
-    server: {
-      allowedHosts: ['c971-97-93-218-63.ngrok-free.app'],
-    },
-  },
-});
+export default defineConfig({});
